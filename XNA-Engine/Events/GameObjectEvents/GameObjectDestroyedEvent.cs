38d6c05f7f01bf7058f374bc82c907475ebf91e2
Joseph Cross <joe.mcross@gmail.com>
2012-05-12 17:02:17 -0400

    Added GameEvent layout

---
+++ XNA-Engine/Events/GameObjectEvents/GameObjectDestroyedEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Engine.Events.GameObjectEvents
+{
+    public class GameObjectDestroyedEvent
+    {
+    }
+}
