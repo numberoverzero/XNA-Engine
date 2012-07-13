e60edfb0d68fda64d071f75f01c6afc47f1df916
Joe Cross <joe.mcross@gmail.com>
2012-07-12 20:11:13 -0700

    Added Event-Driven Input
    Taken from http://stackoverflow.com/a/10222878
    
    This allows us to buffer input for text boxes and such.

---
+++ XNA-Engine/Input/EventInput.cs
@@ -0,0 +1,263 @@
+﻿// Used (with modification) from http://stackoverflow.com/a/10222878
+// SO answer by Niko Drašković
+
+
+#region Using Statements
+
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Text;
+
+#endregion
+
+namespace Engine.Input.EventInput
+{
+
+    public class KeyboardLayout
+    {
+        const uint KLF_ACTIVATE = 1; //activate the layout
+        const int KL_NAMELENGTH = 9; // length of the keyboard buffer
+        const string LANG_EN_US = "00000409";
+        const string LANG_HE_IL = "0001101A";
+
+        [DllImport("user32.dll")]
+        private static extern long LoadKeyboardLayout(
+              string pwszKLID,  // input locale identifier
+              uint Flags       // input locale identifier options
+              );
+
+        [DllImport("user32.dll")]
+        private static extern long GetKeyboardLayoutName(
+              System.Text.StringBuilder pwszKLID  //[out] string that receives the name of the locale identifier
+              );
+
+        public static string getName()
+        {
+            System.Text.StringBuilder name = new System.Text.StringBuilder(KL_NAMELENGTH);
+            GetKeyboardLayoutName(name);
+            return name.ToString();
+        }
+    }
+
+    public class CharacterEventArgs : EventArgs
+    {
+        public char Character { get; private set; }
+        public int Param { get; private set; }
+
+        public CharacterEventArgs(char character, int lParam)
+        {
+            Character = character;
+            Param = lParam;
+        }
+
+        public int RepeatCount
+        {
+            get { return Param & 0xffff; }
+        }
+
+        public bool ExtendedKey
+        {
+            get { return (Param & (1 << 24)) > 0; }
+        }
+
+        public bool AltPressed
+        {
+            get { return (Param & (1 << 29)) > 0; }
+        }
+
+        public bool PreviousState
+        {
+            get { return (Param & (1 << 30)) > 0; }
+        }
+
+        public bool TransitionState
+        {
+            get { return (Param & (1 << 31)) > 0; }
+        }
+    }
+
+    public class KeyEventArgs : EventArgs
+    {
+        public Keys KeyCode { get; private set; }
+
+        public KeyEventArgs(Keys keyCode)
+        {
+            KeyCode = keyCode;
+        }
+
+    }
+
+    public delegate void CharEnteredHandler(object sender, CharacterEventArgs e);
+    public delegate void KeyEventHandler(object sender, KeyEventArgs e);
+
+    public static class EventInput
+    {
+        /// <summary>
+        /// Event raised when a character has been entered.
+        /// </summary>
+        public static event CharEnteredHandler CharEntered;
+
+        /// <summary>
+        /// Event raised when a key has been pressed down. May fire multiple times due to keyboard repeat.
+        /// </summary>
+        public static event KeyEventHandler KeyDown;
+
+        /// <summary>
+        /// Event raised when a key has been released.
+        /// </summary>
+        public static event KeyEventHandler KeyUp;
+
+        delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        static bool initialized;
+        static IntPtr prevWndProc;
+        static WndProc hookProcDelegate;
+        static IntPtr hIMC;
+
+        //various Win32 constants that we need
+        const int GWL_WNDPROC = -4;
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_CHAR = 0x102;
+        const int WM_IME_SETCONTEXT = 0x0281;
+        const int WM_INPUTLANGCHANGE = 0x51;
+        const int WM_GETDLGCODE = 0x87;
+        const int WM_IME_COMPOSITION = 0x10f;
+        const int DLGC_WANTALLKEYS = 4;
+
+        //Win32 functions that we're using
+        [DllImport("Imm32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr ImmGetContext(IntPtr hWnd);
+
+        [DllImport("Imm32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr ImmAssociateContext(IntPtr hWnd, IntPtr hIMC);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+
+        /// <summary>
+        /// Initialize the TextInput with the given GameWindow.
+        /// </summary>
+        /// <param name="window">The XNA window to which text input should be linked.</param>
+        public static void Initialize(GameWindow window)
+        {
+            if (initialized)
+                throw new InvalidOperationException("TextInput.Initialize can only be called once!");
+
+            hookProcDelegate = new WndProc(HookProc);
+            prevWndProc = (IntPtr)SetWindowLong(window.Handle, GWL_WNDPROC,
+                (int)Marshal.GetFunctionPointerForDelegate(hookProcDelegate));
+
+            hIMC = ImmGetContext(window.Handle);
+            initialized = true;
+        }
+
+        static IntPtr HookProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            IntPtr returnCode = CallWindowProc(prevWndProc, hWnd, msg, wParam, lParam);
+
+            switch (msg)
+            {
+                case WM_GETDLGCODE:
+                    returnCode = (IntPtr)(returnCode.ToInt32() | DLGC_WANTALLKEYS);
+                    break;
+
+                case WM_KEYDOWN:
+                    if (KeyDown != null)
+                        KeyDown(null, new KeyEventArgs((Keys)wParam));
+                    break;
+
+                case WM_KEYUP:
+                    if (KeyUp != null)
+                        KeyUp(null, new KeyEventArgs((Keys)wParam));
+                    break;
+
+                case WM_CHAR:
+                    if (CharEntered != null)
+                        CharEntered(null, new CharacterEventArgs((char)wParam, lParam.ToInt32()));
+                    break;
+
+                case WM_IME_SETCONTEXT:
+                    if (wParam.ToInt32() == 1)
+                        ImmAssociateContext(hWnd, hIMC);
+                    break;
+
+                case WM_INPUTLANGCHANGE:
+                    ImmAssociateContext(hWnd, hIMC);
+                    returnCode = (IntPtr)1;
+                    break;
+            }
+
+            return returnCode;
+        }
+    }
+
+    public interface IKeyboardSubscriber
+    {
+        void RecieveTextInput(char inputChar);
+        void RecieveCommandInput(char command);
+        void RecieveSpecialInput(Keys key);
+
+        bool Selected { get; set; } //or Focused
+    }
+
+    public static class KeyboardDispatcher
+    {
+        static bool initialized;
+
+        public static void Initialize(GameWindow window)
+        {
+            if (!initialized)
+            {
+                EventInput.Initialize(window);
+                EventInput.CharEntered += new CharEnteredHandler(EventInput_CharEntered);
+                EventInput.KeyDown += new KeyEventHandler(EventInput_KeyDown);
+                initialized = true;
+            }
+        }
+
+        static void EventInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_subscriber.Count == 0)
+                return;
+
+            foreach(var subscriber in _subscriber)
+                subscriber.RecieveSpecialInput(e.KeyCode);
+        }
+
+        static void EventInput_CharEntered(object sender, CharacterEventArgs e)
+        {
+            if (_subscriber.Count == 0)
+                return;
+            if (char.IsControl(e.Character))
+            {
+                foreach (var subscriber in _subscriber)
+                    subscriber.RecieveCommandInput(e.Character);
+            }
+            else
+            {
+                foreach (var subscriber in _subscriber)
+                    subscriber.RecieveTextInput(e.Character);
+            }
+        }
+
+        static List<IKeyboardSubscriber> _subscriber = new List<IKeyboardSubscriber>();
+
+        public static void RegisterListener(IKeyboardSubscriber subscriber)
+        {
+            _subscriber.Add(subscriber);
+        }
+
+        public static void UnregisterListener(IKeyboardSubscriber subscriber)
+        {
+            _subscriber.Remove(subscriber);
+        }
+    }
+}
