3e9fe80dc221e3ab5051aa8b16a989e51b53bb1b
Joe Cross <joe.mcross@gmail.com>
2012-08-04 06:13:23 -0700

    Added Invocation Extension for sync issues
    Makes it easy to manage controls in threaded apps

---
+++ XNA-Engine/Utility/InvokeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Engine.Utility
+{
+    /// <summary>
+    /// Extensions related to Invocation
+    /// </summary>
+    public static class InvokeExtensions
+    {
+        /// <summary>
+        /// If performing the action would require a synchronized invoke,
+        /// does that for you.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="this"></param>
+        /// <param name="action"></param>
+        public static void InvokeEx<T>(this T @this, Action<T> action) where T : ISynchronizeInvoke
+        {
+            if (@this.InvokeRequired)
+            {
+                @this.Invoke(action, new object[] { @this });
+            }
+            else
+            {
+                action(@this);
+            }
+        }
+    }
+
+}
