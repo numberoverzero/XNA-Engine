7a1317f1244267ba4d86cf9b9df7a79c62377c2f
Joe Cross <joe.mcross@gmail.com>
2012-09-01 08:57:34 -0700

    Added InputDevice
    Tidying up the [Previous/Current][Keyboard/Mouse/GamePad]State nonsense

---
+++ XNA-Engine/Input/Devices/BasicDevices.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Engine.Input.Devices
+{
+    /// <summary>
+    ///   Tracks keyboard state
+    /// </summary>
+    public class KeyboardDevice : InputDevice
+    {
+        /// <summary>
+        ///   KeyboardState for the previous frame
+        /// </summary>
+        public KeyboardState PreviousKeyboardState { get; protected set; }
+
+        /// <summary>
+        ///   KeyboardState for the current frame
+        /// </summary>
+        public KeyboardState CurrentKeyboardState { get; protected set; }
+
+        #region InputDevice Members
+
+        /// <summary>
+        ///   See <see cref="InputDevice.Update" />
+        /// </summary>
+        public void Update()
+        {
+            PreviousKeyboardState = CurrentKeyboardState;
+            CurrentKeyboardState = Keyboard.GetState();
+        }
+
+        /// <summary>
+        ///   See <see cref="InputDevice.GetDeviceSnapshot" />
+        /// </summary>
+        public InputSnapshot GetDeviceSnapshot(PlayerIndex player, FrameState frameState)
+        {
+            var keyboardState = frameState == FrameState.Current ? CurrentKeyboardState : PreviousKeyboardState;
+            return new InputSnapshot(keyboardState, null, null, null);
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    ///   Tracks mouse state
+    /// </summary>
+    public class MouseDevice : InputDevice
+    {
+        /// <summary>
+        ///   MouseState for the previous frame
+        /// </summary>
+        public MouseState PreviousMouseState { get; protected set; }
+
+        /// <summary>
+        ///   MouseState for the current frame
+        /// </summary>
+        public MouseState CurrentMouseState { get; protected set; }
+
+        #region InputDevice Members
+
+        /// <summary>
+        ///   See <see cref="InputDevice.Update" />
+        /// </summary>
+        public void Update()
+        {
+            PreviousMouseState = CurrentMouseState;
+            CurrentMouseState = Mouse.GetState();
+        }
+
+        /// <summary>
+        ///   See <see cref="InputDevice.GetDeviceSnapshot" />
+        /// </summary>
+        public InputSnapshot GetDeviceSnapshot(PlayerIndex player, FrameState frameState)
+        {
+            var mouseState = frameState == FrameState.Current ? CurrentMouseState : PreviousMouseState;
+            return new InputSnapshot(null, null, mouseState, null);
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    ///   Tracks GamePad states
+    /// </summary>
+    public class GamePadDevice : InputDevice
+    {
+        /// <summary>
+        ///   GamePadState for the previous frame
+        /// </summary>
+        public Dictionary<PlayerIndex, GamePadState> PreviousGamePadStates { get; protected set; }
+
+        /// <summary>
+        ///   GamePadState for the current frame
+        /// </summary>
+        public Dictionary<PlayerIndex, GamePadState> CurrentGamePadStates { get; protected set; }
+
+        #region InputDevice Members
+
+        /// <summary>
+        ///   See <see cref="InputDevice.Update" />
+        /// </summary>
+        public void Update()
+        {
+            foreach (var player in Globals.Players)
+            {
+                PreviousGamePadStates[player] = CurrentGamePadStates[player];
+                CurrentGamePadStates[player] = GamePad.GetState(player);
+            }
+        }
+
+        /// <summary>
+        ///   See <see cref="InputDevice.GetDeviceSnapshot" />
+        /// </summary>
+        public InputSnapshot GetDeviceSnapshot(PlayerIndex player, FrameState frameState)
+        {
+            var gamePadState = frameState == FrameState.Current
+                                   ? CurrentGamePadStates[player]
+                                   : PreviousGamePadStates[player];
+            return new InputSnapshot(null, gamePadState, null, null);
+        }
+
+        #endregion
+    }
+}
---
+++ XNA-Engine/Input/Devices/InputDevice.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Engine.Input.Devices
+{
+    /// <summary>
+    ///   Wrapper around different devices, loads info into a snapshot.
+    /// </summary>
+    public interface InputDevice
+    {
+        /// <summary>
+        ///   Update the device
+        /// </summary>
+        void Update();
+
+        /// <summary>
+        ///   Returns a (likely partial) snapshot of the device(s) for a given player
+        /// </summary>
+        /// <returns> </returns>
+        InputSnapshot GetDeviceSnapshot(PlayerIndex player, FrameState frameState);
+    }
+}
