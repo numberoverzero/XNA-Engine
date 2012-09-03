ecb3bdfe423966a3b93890d844a2d144597dfa76
Joe Cross <joe.mcross@gmail.com>
2012-09-02 17:58:32 -0700

    Enhanced BidirectionalDict
    Renamed HasItem, Added Clear.

--- XNA-Engine/DataStructures/BidirectionalDict.cs
+++ XNA-Engine/DataStructures/BidirectionalDict.cs
@@ -1,26 +1,22 @@
-﻿using System;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Collections.Concurrent;
-using System.Linq;
-using System.Text;
-
 using Engine.Utility;
 
 namespace Engine.DataStructures
 {
     /// <summary>
-    /// Provides strict 1:1 mapping between two types.
-    /// Not thread safe by any stretch of the imagination
+    ///   Provides strict 1:1 mapping between two types.
+    ///   Not thread safe by any stretch of the imagination
     /// </summary>
-    /// <typeparam name="T1"></typeparam>
-    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T1"> </typeparam>
+    /// <typeparam name="T2"> </typeparam>
     public class BidirectionalDict<T1, T2>
     {
-        ConcurrentDictionary<T1, T2> t1_t2;
-        ConcurrentDictionary<T2, T1> t2_t1;
+        private readonly ConcurrentDictionary<T1, T2> t1_t2;
+        private readonly ConcurrentDictionary<T2, T1> t2_t1;
 
         /// <summary>
-        /// Empty Bidirectional Dictionary
+        ///   Empty Bidirectional Dictionary
         /// </summary>
         public BidirectionalDict()
         {
@@ -29,9 +25,9 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// Construct the equivalent bi-directional dictionary from a single-direction dictionary
+        ///   Construct the equivalent bi-directional dictionary from a single-direction dictionary
         /// </summary>
-        /// <param name="dictionary"></param>
+        /// <param name="dictionary"> </param>
         public BidirectionalDict(IDictionary<T1, T2> dictionary)
         {
             t1_t2 = new ConcurrentDictionary<T1, T2>(dictionary);
@@ -41,9 +37,9 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// Construct the equivalent bi-directional dictionary from a T2 -> T1 dictionary
+        ///   Construct the equivalent bi-directional dictionary from a T2 -> T1 dictionary
         /// </summary>
-        /// <param name="dictionary"></param>
+        /// <param name="dictionary"> </param>
         public BidirectionalDict(IDictionary<T2, T1> dictionary)
         {
             t2_t1 = new ConcurrentDictionary<T2, T1>(dictionary);
@@ -53,16 +49,13 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// Gets or sets the values associated with the specified key
+        ///   Gets or sets the values associated with the specified key
         /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
+        /// <param name="key"> </param>
+        /// <returns> </returns>
         public T1 this[T2 key]
         {
-            get
-            {
-                return t2_t1[key];
-            }
+            get { return t2_t1[key]; }
             set
             {
                 t2_t1[key] = value;
@@ -71,16 +64,13 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// Gets or sets the values associated with the specified key
+        ///   Gets or sets the values associated with the specified key
         /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
+        /// <param name="key"> </param>
+        /// <returns> </returns>
         public T2 this[T1 key]
         {
-            get
-            {
-                return t1_t2[key];
-            }
+            get { return t1_t2[key]; }
             set
             {
                 t1_t2[key] = value;
@@ -89,36 +79,38 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// Add the specified pairing to the dictionary
+        ///   Add the specified pairing to the dictionary
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
+        /// <param name="key"> </param>
+        /// <param name="value"> </param>
         public void Add(T1 key, T2 value)
         {
             this[key] = value;
         }
+
         /// <summary>
-        /// Add the specified pairing to the dictionary
+        ///   Add the specified pairing to the dictionary
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
+        /// <param name="key"> </param>
+        /// <param name="value"> </param>
         public void Add(T2 key, T1 value)
         {
             Add(value, key);
         }
 
         /// <summary>
-        /// Remove the item and its corresponding value from the dictionary
+        ///   Remove the item and its corresponding value from the dictionary
         /// </summary>
-        /// <param name="k"></param>
+        /// <param name="k"> </param>
         public void Remove(T1 k)
         {
             Remove(k, this[k]);
         }
+
         /// <summary>
-        /// Remove the item and its corresponding value from the dictionary
+        ///   Remove the item and its corresponding value from the dictionary
         /// </summary>
-        /// <param name="k"></param>
+        /// <param name="k"> </param>
         public void Remove(T2 k)
         {
             Remove(this[k], k);
@@ -134,42 +126,50 @@ namespace Engine.DataStructures
         }
 
         /// <summary>
-        /// True if there is a pairing with the given value
+        ///   True if there is a pairing with the given value
         /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public bool HasItem(T1 key)
+        /// <param name="key"> </param>
+        /// <returns> </returns>
+        public bool Contains(T1 key)
         {
             return t1_t2.ContainsKey(key);
         }
+
         /// <summary>
-        /// True if there is a pairing with the given value
+        ///   True if there is a pairing with the given value
         /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
-        public bool HasItem(T2 key)
+        /// <param name="key"> </param>
+        /// <returns> </returns>
+        public bool Contains(T2 key)
         {
             return t2_t1.ContainsKey(key);
         }
 
         /// <summary>
-        /// Returns the values of the first type specification in the table
+        ///   Clears all values
         /// </summary>
-        /// <returns></returns>
+        public void Clear()
+        {
+            t1_t2.Clear();
+            t2_t1.Clear();
+        }
+
+        /// <summary>
+        ///   Returns the values of the first type specification in the table
+        /// </summary>
+        /// <returns> </returns>
         public IEnumerable<T1> GetValuesType1()
         {
             return t1_t2.Keys;
         }
 
         /// <summary>
-        /// Returns the values of the second type specification in the table
+        ///   Returns the values of the second type specification in the table
         /// </summary>
-        /// <returns></returns>
+        /// <returns> </returns>
         public IEnumerable<T2> GetValuesType2()
         {
             return t2_t1.Keys;
         }
-
-        
     }
 }
