cc7b0eb9c5eb201d4e28bcfcadef11852535cbd5
Joe Cross <joe.mcross@gmail.com>
2012-09-24 00:37:19 -0700

    ReSharper Formatting: ColorTextureGenerator

--- XNA-Engine/Utility/ColorTextureGenerator.cs
+++ XNA-Engine/Utility/ColorTextureGenerator.cs
@@ -1,56 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine.Utility
 {
     /// <summary>
-    /// Creates single color textures (i.e. for drawing rectangles).
+    ///   Creates single color textures (i.e. for drawing rectangles).
     /// </summary>
     public class ColorTextureGenerator
     {
         /// <summary>
-        /// Creates a 1x1 pixel black texture.
+        ///   Creates a 1x1 pixel blank texture.
         /// </summary>
-        /// <param name="graphicsDevice">The graphics device to use.</param>
-        /// <returns>The newly created texture.</returns>
+        /// <param name="graphicsDevice"> The graphics device to use. </param>
+        /// <returns> The newly created texture. </returns>
         public static Texture2D Create(GraphicsDevice graphicsDevice)
         {
             return Create(graphicsDevice, 1, 1, new Color());
         }
 
         /// <summary>
-        /// Creates a 1x1 pixel texture of the specified color.
+        ///   Creates a 1x1 pixel texture of the specified color.
         /// </summary>
-        /// <param name="graphicsDevice">The graphics device to use.</param>
-        /// <param name="color">The color to set the texture to.</param>
-        /// <returns>The newly created texture.</returns>
+        /// <param name="graphicsDevice"> The graphics device to use. </param>
+        /// <param name="color"> The color to set the texture to. </param>
+        /// <returns> The newly created texture. </returns>
         public static Texture2D Create(GraphicsDevice graphicsDevice, Color color)
         {
             return Create(graphicsDevice, 1, 1, color);
         }
 
         /// <summary>
-        /// Creates a texture of the specified color.
+        ///   Creates a texture of the specified color.
         /// </summary>
-        /// <param name="graphicsDevice">The graphics device to use.</param>
-        /// <param name="width">The width of the texture.</param>
-        /// <param name="height">The height of the texture.</param>
-        /// <param name="color">The color to set the texture to.</param>
-        /// <returns>The newly created texture.</returns>
+        /// <param name="graphicsDevice"> The graphics device to use. </param>
+        /// <param name="width"> The width of the texture. </param>
+        /// <param name="height"> The height of the texture. </param>
+        /// <param name="color"> The color to set the texture to. </param>
+        /// <returns> The newly created texture. </returns>
         public static Texture2D Create(GraphicsDevice graphicsDevice, int width, int height, Color color)
         {
             // create the rectangle texture without colors
-            Texture2D texture = new Texture2D(graphicsDevice, width, height);
+            var texture = new Texture2D(graphicsDevice, width, height);
 
             // Create a color array for the pixels
-            Color[] colors = new Color[width * height];
+            var colors = new Color[width*height];
             for (int i = 0; i < colors.Length; i++)
             {
-                colors[i] = new Color(color.ToVector3());
+                colors[i] = new Color(color.ToVector4());
             }
 
             // Set the color data for the texture
