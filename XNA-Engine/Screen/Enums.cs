774a9c33f0a87128c65e20e36ba0026daeeda310
Joe Cross <joe.mcross@gmail.com>
2012-10-06 11:23:30 -0700

    Initial ui attempt
    This is all probably going to change, but it's a start as far as I've
    gotten.

---
+++ XNA-Engine/Screen/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace Engine.Screen
+{
+    /// <summary>
+    /// Enum describes the screen transition state.
+    /// </summary>
+    public enum ScreenState
+    {
+        TransitionOn,
+        Active,
+        TransitionOff,
+        Hidden,
+    }
+
+    public enum OptionLayout
+    {
+        Horizontal,
+        Vertical
+    }
+}
