74783a1083c33b1e528cce338ed4a068aa234534
Joe Cross <joe.mcross@gmail.com>
2012-10-27 17:27:11 -0700

    Resharper formatting

--- XNA-Engine/Utility/ThreadExtensions.cs
+++ XNA-Engine/Utility/ThreadExtensions.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 
 namespace Engine.Utility
 {
     /// <summary>
-    /// Extensions for thread actions
+    ///   Extensions for thread actions
     /// </summary>
     public static class ThreadExtensions
     {
         /// <summary>
-        /// Abort a thread if it doesn't join n milliseconds after an interrupt.
+        ///   Abort a thread if it doesn't join n milliseconds after an interrupt.
         /// </summary>
-        /// <param name="thread"></param>
-        /// <param name="millisecondsTimeout"></param>
+        /// <param name="thread"> </param>
+        /// <param name="millisecondsTimeout"> </param>
         public static void Kill(this Thread thread, int millisecondsTimeout = 0)
         {
             thread.Interrupt();
-            if (!thread.Join(millisecondsTimeout))
+            if (thread.Join(millisecondsTimeout)) return;
+            try
             {
-                try
-                {
-                    thread.Abort();
-                }
-                catch (ThreadAbortException)
-                {
-                }
-                catch (ThreadInterruptedException)
-                {
-                }
+                thread.Abort();
+            }
+            catch (ThreadAbortException)
+            {
+            }
+            catch (ThreadInterruptedException)
+            {
             }
         }
     }
