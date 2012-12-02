b8bc327269e3a7452c2fe06a6927d7b64052083d
Joe Cross <joe.mcross@gmail.com>
2012-12-01 17:45:42 -0800

    Frame/Container dummy methods
    Not implemented yet, commited so we can build

--- XNA-Engine/UI/Container.cs
+++ XNA-Engine/UI/Container.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine.UI
 {
     public class Container : Widget
     {
+        public Container(string name, float x = 0, float y = 0, float w = 0, float h = 0, bool expandable = true, Alignment? align = null) : base(name, x, y, w, h, expandable, align)
+        {
+        }
 
+        protected override void DetermineSize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(SpriteBatch batch)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- XNA-Engine/UI/Frame.cs
+++ XNA-Engine/UI/Frame.cs
@@ -8,5 +8,9 @@ namespace Engine.UI
     public class Frame : Container
     {
         public Dictionary<string, Widget> Names;
+
+        public Frame(string name, float x = 0, float y = 0, float w = 0, float h = 0, bool expandable = true, Alignment? align = null) : base(name, x, y, w, h, expandable, align)
+        {
+        }
     }
 }
