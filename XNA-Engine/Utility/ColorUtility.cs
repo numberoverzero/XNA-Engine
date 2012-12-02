20544347e1d193c0e049476db80635728ad6ad22
Joe Cross <joe.mcross@gmail.com>
2012-12-01 17:43:53 -0800

    String->XnaColor interpolation

---
+++ XNA-Engine/Utility/ColorUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using Microsoft.Xna.Framework;
+
+namespace Engine.Utility
+{
+    public static class ColorUtility
+    {
+        public static Microsoft.Xna.Framework.Color AsColor(this string color)
+        {
+            var sdColor = System.Drawing.Color.FromName(color);
+            return new Microsoft.Xna.Framework.Color(sdColor.R, sdColor.G, sdColor.B, sdColor.A);
+        }
+    }
+}
