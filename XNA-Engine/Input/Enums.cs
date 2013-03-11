462261fb3d400903218dcac1c1178a6c935df268
Joe Cross <joe.mcross@gmail.com>
2013-03-10 18:04:26 -0700

    CLeaned up boring comments
    Dropped the ModifierCheckType enum since we only use strict checks for
    now

--- XNA-Engine/Input/Enums.cs
+++ XNA-Engine/Input/Enums.cs
@@ -9,80 +9,35 @@ namespace Engine.Input
     /// Used to specify the left or right mouse button
     /// </summary>
     public enum MouseButton { 
-        /// <summary>
-        /// No mouse button
-        /// </summary>
         None, 
-        /// <summary>
-        /// Left mouse button
-        /// </summary>
         Left, 
-        /// <summary>
-        /// Right mouse button
-        /// </summary>
         Right, 
-        /// <summary>
-        /// Middle click (not scroll up/down)
-        /// </summary>
         Middle }
 
     /// <summary>
     /// Used to specify the left or right trigger
     /// </summary>
     public enum Trigger { 
-        /// <summary>
-        /// No trigger
-        /// </summary>
         None, 
-        /// <summary>
-        /// Left trigger (as viewed from top)
-        /// </summary>
         Left, 
-        /// <summary>
-        /// Right trigger (as viewed from top)
-        /// </summary>
         Right }
 
     /// <summary>
     /// Used to specify the left or right thumbstick
     /// </summary>
     public enum Thumbstick { 
-        /// <summary>
-        /// No thumbstick
-        /// </summary>
         None, 
-        /// <summary>
-        /// Left thumbstick (as viewed from top)
-        /// </summary>
         Left, 
-        /// <summary>
-        /// Right thumbstick (as viewed from top)
-        /// </summary>
         Right }
 
     /// <summary>
     /// Used to specify the direction of a thumbstick "press" (=1)
     /// </summary>
     public enum ThumbstickDirection { 
-        /// <summary>
-        /// No direction
-        /// </summary>
         None, 
-        /// <summary>
-        /// Controller up
-        /// </summary>
         Up, 
-        /// <summary>
-        /// Controller down
-        /// </summary>
         Down, 
-        /// <summary>
-        /// Controller left
-        /// </summary>
         Left, 
-        /// <summary>
-        /// Controller right
-        /// </summary>
         Right }
 
     /// <summary>
@@ -98,24 +53,4 @@ namespace Engine.Input
         /// The frame that logic is being run in right now (or graphics are being rendered in, depending on context)
         /// </summary>
         Current }
-
-    /// <summary>
-    /// How a manager interpolates binding checks (if at all)
-    /// </summary>
-    public enum ModifierCheckType
-    {
-        /// <summary>
-        /// The exact modifiers specified by the binding must be active.  No other modifiers may be active.
-        /// </summary>
-        Strict,
-
-        /// <summary>
-        /// Binding A with modifiers x, y will be counted active (even if modifier z is active) so long as there is no other binding B with modifiers x, y, and z.
-        /// </summary>
-        /// <example>
-        /// Jump is bound to space bar.  Fireball is 1, Fire nova is Shift + 1.
-        /// In strict mode, Shift + Space would not jump because there are active modifiers not in the jump binidng (space w/o mods)
-        /// In smart mode, there are no bindings with Shift + Space, so it falls back to the base binding, Jump.
-        /// </example>
-        Smart }
 }
--- XNA-Engine/Input/InputSettings.cs
+++ XNA-Engine/Input/InputSettings.cs
@@ -20,24 +20,16 @@ namespace Engine.Input
         public float ThumbstickThreshold;
 
         /// <summary>
-        ///   How a binding's modifiers are validated against the manager state.
-        ///   See <see cref="ModifierCheckType" /> for more info.
-        /// </summary>
-        public ModifierCheckType ModifierCheckType;
-
-        /// <summary>
         /// Construct a set of Input Settings describing certain thresholds and limits that must be met for
         /// some bindings to be considered "active"
         /// </summary>
         /// <param name="triggerThreshold">The minimum value a trigger must have to be considered pulled/depressed (active)</param>
         /// <param name="thumbstickThreshold">The minimum value a thumbstick must be moved to be considered active 
         /// (some bindings may require this amount in a single direction)</param>
-        /// <param name="modifierCheckType">How modifiers are checked</param>
-        public InputSettings(float triggerThreshold, float thumbstickThreshold, ModifierCheckType modifierCheckType)
+        public InputSettings(float triggerThreshold, float thumbstickThreshold)
         {
             TriggerThreshold = triggerThreshold;
             ThumbstickThreshold = thumbstickThreshold;
-            ModifierCheckType = modifierCheckType;
         }
 
         /// <summary>
@@ -48,7 +40,6 @@ namespace Engine.Input
         {
             TriggerThreshold = settings.TriggerThreshold;
             ThumbstickThreshold = settings.ThumbstickThreshold;
-            ModifierCheckType = settings.ModifierCheckType;
         }
     }
 }
